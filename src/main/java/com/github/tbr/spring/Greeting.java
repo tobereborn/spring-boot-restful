f76dcee9c34628991d3bb663baf617128f2498ee
none <none>
2017-09-16 22:34:35 +0800

    Add init  hello world

---
+++ src/main/java/com/github/tbr/spring/Greeting.java
@@ -0,0 +1,45 @@
+package com.github.tbr.spring;
+
+public class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Greeting greeting = (Greeting) o;
+
+        if (id != greeting.id) return false;
+        return content != null ? content.equals(greeting.content) : greeting.content == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (content != null ? content.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Greeting{" +
+                "id=" + id +
+                ", content='" + content + '\'' +
+                '}';
+    }
+}
