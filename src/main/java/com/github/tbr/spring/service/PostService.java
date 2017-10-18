5493c7ca3700c8ffe59984e99b786768aa028dd5
none <none>
2017-10-18 23:54:53 +0800

    Pageable

--- src/main/java/com/github/tbr/spring/service/PostService.java
+++ src/main/java/com/github/tbr/spring/service/PostService.java
@@ -4,6 +4,10 @@ package com.github.tbr.spring.service;
 import com.github.tbr.spring.domain.Post;
 import com.github.tbr.spring.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -23,5 +27,11 @@ public class PostService {
     @Transactional(readOnly = true)
     public List<Post> getAllPosts() {
         return postRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Page<Post> getPageablePosts(int page, int size) {
+        Pageable pageable = new PageRequest(page, size, Sort.Direction.DESC, "id");
+        return postRepository.findAll(pageable);
     }
 }
--- src/main/java/com/github/tbr/spring/web/PostController.java
+++ src/main/java/com/github/tbr/spring/web/PostController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/api")
@@ -16,9 +17,14 @@ public class PostController {
     private PostService postService;
 
     @GetMapping("/posts")
-    public String getPost(Model model) {
+    public String getAllPosts(Model model) {
         model.addAttribute(postService.getAllPosts());
         return "posts";
+    }
 
+    @GetMapping("/pages")
+    public String getPostPages(@RequestParam(name = "page") int page, @RequestParam(name = "size") int size, Model model) {
+        model.addAttribute("pages", postService.getPageablePosts(page, size));
+        return "pages";
     }
 }
